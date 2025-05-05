ab24b2c9f81af08e00e97affbf7ece19ccb805ea
Kallst <santiago.castrom@autonoma.edu.co>
2025-05-05 17:26:51 -0500

    Se creo la clase PulgaNormal que hereda de la clase Pulga

---
+++ SimuladorPulgas/src/autonoma/simuladorpulgas/models/PulgaNormal.java
@@ -0,0 +1,19 @@
+package autonoma.simuladorpulgas.models;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public class PulgaNormal extends Pulga {
+
+    public PulgaNormal(int vida, int posicionX, int posicionY) {
+        super(vida, posicionX, posicionY);
+    }
+
+    @Override
+    public void reubicar(int x, int y) {
+        
+    }
+    
+    
+}
