cdb8070ec299c11f2100047648ba71f63f17e0d2
Kallst <santiago.castrom@autonoma.edu.co>
2025-05-05 17:22:05 -0500

    Se agregó la posición de la pulga

--- SimuladorPulgas/src/autonoma/simuladorpulgas/models/Pulga.java
+++ SimuladorPulgas/src/autonoma/simuladorpulgas/models/Pulga.java
@@ -6,6 +6,15 @@ package autonoma.simuladorpulgas.models;
  */
 public abstract class Pulga {
     protected int vida;
+    protected int x;
+    protected int y;
+
+    public Pulga(int vida, int posicionX, int posicionY) {
+        this.vida = vida;
+        this.x = posicionX;
+        this.y = posicionY;
+    }
+   
     
-    public abstract void reubicar ();
+    public abstract void reubicar (int x , int y);
 }
